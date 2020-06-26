89a2ad8dee8db3383d2010553d70a0c21f61e5c8
bostoncleek <bocl8874@colorado.edu>
2020-06-26 18:16:14 -0500

    Changed frame the grasps are drawn in
    Drawing the grasps in the robot's base_link frame to verify
    the correct pose goal is sent to the arm. However, the arm
    currently doesn't find a path to the goal.

--- src/gpd_simulation_node.cpp
+++ src/gpd_simulation_node.cpp
@@ -11,13 +11,13 @@
 // ROS
 #include <ros/ros.h>
 #include <ros/console.h>
+#include <eigen_conversions/eigen_msg.h>
 #include <std_srvs/Empty.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <tf2_ros/transform_listener.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <geometry_msgs/Twist.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 // MoveIt
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
@@ -30,6 +30,7 @@
 // C++
 #include <vector>
 #include <iostream>
+#include <string>
 
 // PCL
 #include <pcl/common/common.h>
@@ -43,18 +44,51 @@
 #include <pcl/filters/extract_indices.h>
 #include <pcl/visualization/cloud_viewer.h>
 
+// Eigen
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
 // GPD
-#include <gpd_ros/CloudSources.h>
-#include <gpd_ros/CloudSamples.h>
+// #include <gpd_ros/CloudSources.h>
+// #include <gpd_ros/CloudSamples.h>
 #include <gpd_ros/GraspConfig.h>
 #include <gpd_ros/GraspConfigList.h>
 
 
+struct Gripper
+{
+  // Translation from frame to gripper
+  Eigen::Vector3d position;
+
+  // Orientation of gripper, R = [approach binormal axis]
+  Eigen::Quaterniond quat;
+  Eigen::Vector3d approach;
+  Eigen::Vector3d binormal;
+  Eigen::Vector3d axis;
+
+  float width;
+  float score;
+
+  std::string frame_id;
+};
+
+
+
 static sensor_msgs::PointCloud2 cloud_msg;                      // point cloud to search for graps
-static geometry_msgs::TransformStamped t_stamped_base_cam;      // Transform from robot base_link to camera_optical_link
+static geometry_msgs::TransformStamped t_stamped_base_opt;      // Transform from robot base_link to camera_optical_link
+static geometry_msgs::PoseStamped grasp_pose_robot;             // grasp pose goal
+static Gripper grasp_candidate;                                 // best grasp candidate
 
 static bool cloud_srv_active;                                   // request grasps status
 static bool cloud_msg_received;                                 // point cloud message received status
+static bool grasp_selected;                                     // publish selected grasp
+
+
+// TODO read params in through server
+static const auto outer_diameter = 0.12;
+static const auto hand_depth = 0.06;
+static const auto finger_width = 0.01;
+static const auto hand_height = 0.02;
 
 
 
@@ -111,6 +145,7 @@ void removeTable(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
 
 bool requestGraspCloudCallBack(std_srvs::Empty::Request&, std_srvs::Empty::Response&)
 {
+  ROS_INFO("Request grasp service activated");
   cloud_srv_active = true;
   return true;
 }
@@ -148,35 +183,176 @@ void cloudCallBack(const sensor_msgs::PointCloud2::ConstPtr &msg)
 }
 
 
+
 void graspCallBack(const gpd_ros::GraspConfigList::ConstPtr &msg)
 {
-  ROS_INFO("number of graps received %lu", msg->grasps.size());
+  ROS_INFO("Number of graps received %lu", msg->grasps.size());
 
-  // Input grasp position in optical frame of camera
-  geometry_msgs::PointStamped p_in;
-  p_in.point = msg->grasps.at(0).position;
+  // Use the best grasp
+  // grasp_msg = msg->grasps.at(0);
 
-  // Output grasp position in frame of the base_link of the robot
-  geometry_msgs::PointStamped p_out;
+  // Position of grasp
+  Eigen::Vector3d position;
+  tf::pointMsgToEigen(msg->grasps.at(0).position, position);
 
-  // Transform grasp position into frame of the base_link of the robot
-  tf2::doTransform(p_in, p_out, t_stamped_base_cam);
+  // Orientation of grasp
+  Eigen::Vector3d approach;
+  Eigen::Vector3d binormal;
+  Eigen::Vector3d axis;
+  tf::vectorMsgToEigen(msg->grasps.at(0).approach, approach);
+  tf::vectorMsgToEigen(msg->grasps.at(0).binormal, binormal);
+  tf::vectorMsgToEigen(msg->grasps.at(0).axis, axis);
+
+  Eigen::Matrix3d orientation;
+  orientation.col(0) = approach;
+  orientation.col(1) = binormal;
+  orientation.col(2) = axis;
+
+  // Transform from camera optical link to grasp
+  Eigen::Affine3d t_optical_grasp;
+  t_optical_grasp.translation() = position;
+  t_optical_grasp.linear() = orientation;
+
+  // std::cout << "Translation: " <<  t_optical_grasp.translation() << std::endl;
+  // std::cout << "Rotation: " << t_optical_grasp.rotation() << std::endl;
+
+  // Convert Eigen transform into pose msg in frame of optical link
+  geometry_msgs::Pose pose_frame_optical;
+  tf::poseEigenToMsg(t_optical_grasp, pose_frame_optical);
+
+  geometry_msgs::PoseStamped grasp_pose_optical;
+  grasp_pose_optical.pose = pose_frame_optical;
+
+  // Transform grasp pose in frame of optical link into frame of the base_link of the robot
+  // geometry_msgs::PoseStamped grasp_pose_robot;
+  tf2::doTransform(grasp_pose_optical, grasp_pose_robot, t_stamped_base_opt);
+
+  std::cout << "Pose of grasp in base_link frame: " << grasp_pose_robot << std::endl;
 
 
-  std::cout << "Position of grasp: " << p_out << std::endl;
+  // Convert to representation used to visualize in frame of robot's base_link
+  tf::quaternionMsgToEigen(grasp_pose_robot.pose.orientation, grasp_candidate.quat);
+  Eigen::Matrix3d grasp_rot = grasp_candidate.quat.toRotationMatrix();
+
+  tf::pointMsgToEigen(grasp_pose_robot.pose.position, grasp_candidate.position);
+  grasp_candidate.approach = grasp_rot.col(0);
+  grasp_candidate.binormal = grasp_rot.col(1);
+  grasp_candidate.axis = grasp_rot.col(2);
+
+  grasp_candidate.width = msg->grasps.at(0).width.data;
+  grasp_candidate.score = msg->grasps.at(0).score.data;
+  grasp_candidate.frame_id = grasp_pose_robot.header.frame_id;
+
+  grasp_selected = true;
+}
 
 
-  std::cout << "approach of grasp: " << msg->grasps.at(0).approach << std::endl;
-  std::cout << "binormal of grasp: " << msg->grasps.at(0).binormal << std::endl;
-  std::cout << "axis of grasp: " << msg->grasps.at(0).axis << std::endl;
+visualization_msgs::Marker fingerMarker(const Eigen::Vector3d& center,
+  const Eigen::Quaterniond quat, const Eigen::Vector3d& lwh, int id, const std::string& frame_id)
+{
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = frame_id;
+  marker.header.stamp = ros::Time();
+  marker.ns = "finger";
+  marker.id = id;
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker.pose.position.x = center(0);
+  marker.pose.position.y = center(1);
+  marker.pose.position.z = center(2);
+  marker.lifetime = ros::Duration(100);
+
+  // use orientation of hand
+  marker.pose.orientation.x = quat.x();
+  marker.pose.orientation.y = quat.y();
+  marker.pose.orientation.z = quat.z();
+  marker.pose.orientation.w = quat.w();
+
+  // these scales are relative to the hand frame (unit: meters)
+  marker.scale.x = lwh(0); // forward direction
+  marker.scale.y = lwh(1); // hand closing direction
+  marker.scale.z = lwh(2); // hand vertical direction
+
+  marker.color.a = 0.5;
+  marker.color.r = 0.0;
+  marker.color.g = 0.5;
+  marker.color.b = 0.0;
+
+  return marker;
+}
 
 
-  // for(unsigned int i = 0; i < msg->grasps.size(); i++)
-  // {
-  //   std::cout << " Grasp: " << i \
-  //             << " Score: " << msg->grasps.at(i).score \
-  //             << " Position: " << msg->grasps.at(i).position << std::endl;
-  // }
+visualization_msgs::Marker handBaseMarker(const Eigen::Vector3d& start,
+  const Eigen::Vector3d& end, const Eigen::Quaterniond quat, double length, double height, int id,
+  const std::string& frame_id)
+{
+  Eigen::Vector3d center = start + 0.5 * (end - start);
+
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = frame_id;
+  marker.header.stamp = ros::Time();
+  marker.ns = "hand_base";
+  marker.id = id;
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker.pose.position.x = center(0);
+  marker.pose.position.y = center(1);
+  marker.pose.position.z = center(2);
+  marker.lifetime = ros::Duration(100);
+
+  // use orientation of hand
+  marker.pose.orientation.x = quat.x();
+  marker.pose.orientation.y = quat.y();
+  marker.pose.orientation.z = quat.z();
+  marker.pose.orientation.w = quat.w();
+
+  // these scales are relative to the hand rotation (unit: meters)
+  marker.scale.x = length; // forward direction
+  marker.scale.y = (end - start).norm(); // hand closing direction
+  marker.scale.z = height; // hand vertical direction
+
+  marker.color.a = 0.5;
+  marker.color.r = 0.0;
+  marker.color.g = 1.0;
+  marker.color.b = 0.0;
+
+  return marker;
+}
+
+
+void visualizeGraspCandidate(visualization_msgs::MarkerArray &marker_array)
+{
+  // use the first id which is the best hand
+  const auto i = 0;
+
+  const auto hw = 0.5*outer_diameter - 0.5*finger_width;
+
+  visualization_msgs::Marker left_finger, right_finger, base, approach;
+  Eigen::Vector3d left_bottom, right_bottom, left_top, right_top, left_center, right_center, approach_center, base_center;
+
+  left_bottom = grasp_candidate.position - hw * grasp_candidate.binormal;
+  right_bottom = grasp_candidate.position + hw * grasp_candidate.binormal;
+  left_top = left_bottom + hand_depth * grasp_candidate.approach;
+  right_top = right_bottom + hand_depth * grasp_candidate.approach;
+  left_center = left_bottom + 0.5*(left_top - left_bottom);
+  right_center = right_bottom + 0.5*(right_top - right_bottom);
+  base_center = left_bottom + 0.5*(right_bottom - left_bottom) - 0.01*grasp_candidate.approach;
+  approach_center = base_center - 0.04*grasp_candidate.approach;
+
+  Eigen::Vector3d finger_lwh, approach_lwh;
+  finger_lwh << hand_depth, finger_width, hand_height;
+  approach_lwh << 0.08, finger_width, hand_height;
+
+
+  base = handBaseMarker(left_bottom, right_bottom, grasp_candidate.quat, 0.02, hand_height, i, grasp_candidate.frame_id);
+  left_finger = fingerMarker(left_center, grasp_candidate.quat, finger_lwh, i*3, grasp_candidate.frame_id);
+  right_finger = fingerMarker(right_center, grasp_candidate.quat, finger_lwh, i*3+1, grasp_candidate.frame_id);
+  approach = fingerMarker(approach_center, grasp_candidate.quat, approach_lwh, i*3+2, grasp_candidate.frame_id);
+
+  marker_array.markers.push_back(left_finger);
+  marker_array.markers.push_back(right_finger);
+  marker_array.markers.push_back(approach);
+  marker_array.markers.push_back(base);
 }
 
 
@@ -190,8 +366,8 @@ int main(int argc, char** argv)
   ros::Subscriber cloud_sub = node_handle.subscribe("camera/depth/points", 1, cloudCallBack);
   // ros::Subscriber cloud_sub = node_handle.subscribe("depth_camera/depth/points", 1, cloudCallBack);
 
-
   ros::Subscriber grasp_sub = node_handle.subscribe("detect_grasps/clustered_grasps", 1, graspCallBack);
+  ros::Publisher grasp_viz_pub = node_handle.advertise<visualization_msgs::MarkerArray>("grasp_candidate", 1);
   ros::Publisher cloud_pub = node_handle.advertise<sensor_msgs::PointCloud2>("cloud_stitched", 1);
   ros::ServiceServer cloud_serv = node_handle.advertiseService("request_grasp", requestGraspCloudCallBack);
 
@@ -202,48 +378,49 @@ int main(int argc, char** argv)
 
   cloud_srv_active = false;
   cloud_msg_received = false;
+  grasp_selected = false;
 
-  // // Required
-  // ros::AsyncSpinner spinner(1);
-  // spinner.start();
-  //
-  // // Move group interface
-  // static const std::string PLANNING_GROUP = "manipulator";
-  // static const std::string END_EFFECTOR_LINK = "ee_link";
-  //
-  // moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-  // move_group.setEndEffectorLink(END_EFFECTOR_LINK);
-  //
-  // const moveit::core::JointModelGroup* joint_model_group =
-  //      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-  //
-  // // Setup visualization
-  // namespace rvt = rviz_visual_tools;
-  // moveit_visual_tools::MoveItVisualTools visual_tools("shoulder_link");
-  // visual_tools.deleteAllMarkers();
-  // visual_tools.loadRemoteControl();
-  //
-  // Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
-  // text_pose.translation().z() = 1.75;
-  // visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
-  //
-  // visual_tools.trigger();
-  //
-  // // Reference frame for this robot.
-  // ROS_INFO_NAMED("GPD", "Planning frame: %s", move_group.getPlanningFrame().c_str());
-  //
-  // // End-effector link for this group.
-  // ROS_INFO_NAMED("GPD", "End effector link: %s", move_group.getEndEffectorLink().c_str());
-  //
-  // // List of all the groups in the robot:
-  // ROS_INFO_NAMED("GPD", "Available Planning Groups:");
-  // std::copy(move_group.getJointModelGroupNames().begin(), move_group.getJointModelGroupNames().end(),
-  //           std::ostream_iterator<std::string>(std::cout, ", "));
-  //
+  // Required
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  // Move group interface
+  static const std::string PLANNING_GROUP = "manipulator";
+  static const std::string END_EFFECTOR_LINK = "ee_link";
+
+  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+  move_group.setEndEffectorLink(END_EFFECTOR_LINK);
+
+  const moveit::core::JointModelGroup* joint_model_group =
+       move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  // Setup visualization
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("shoulder_link");
+  visual_tools.deleteAllMarkers();
+  visual_tools.loadRemoteControl();
+
+  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+  text_pose.translation().z() = 1.75;
+  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+
+  visual_tools.trigger();
+
+  // Reference frame for this robot.
+  ROS_INFO_NAMED("GPD", "Planning frame: %s", move_group.getPlanningFrame().c_str());
+
+  // End-effector link for this group.
+  ROS_INFO_NAMED("GPD", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+
+  // List of all the groups in the robot:
+  ROS_INFO_NAMED("GPD", "Available Planning Groups:");
+  std::copy(move_group.getJointModelGroupNames().begin(), move_group.getJointModelGroupNames().end(),
+            std::ostream_iterator<std::string>(std::cout, ", "));
+
   // visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to move to the pose goal");
   //
-  // // geometry_msgs::PoseStamped eef_link_pose = move_group.getCurrentPose();
-  // // std::cout << eef_link_pose << std::endl;
+  // geometry_msgs::PoseStamped eef_link_pose = move_group.getCurrentPose();
+  // std::cout << eef_link_pose << std::endl;
   //
   // // Plan to a pose goal
   // geometry_msgs::Pose target_pose1;
@@ -271,34 +448,77 @@ int main(int argc, char** argv)
   // // Move to the goal
   // move_group.move();
 
+  spinner.stop();
 
-
+  ROS_INFO("Waiting for grasp candidate... ");
   while(node_handle.ok())
   {
     ros::spinOnce();
 
-    // TODO: to this once at start up
     try
     {
-      t_stamped_base_cam = tfBuffer.lookupTransform("base_link", "camera_rgb_optical_frame", ros::Time(0));
-      // std::cout << t_stamped_base_cam << std::endl;
+      // ROS_INFO("Looking up transform between base_link and camera_rgb_optical_frame...");
+      t_stamped_base_opt = tfBuffer.lookupTransform("base_link", "camera_rgb_optical_frame", ros::Time(2.0));
     }
 
     catch (tf2::TransformException &ex)
     {
       ROS_WARN("%s", ex.what());
-      ros::Duration(1.0).sleep();
-      continue;
+      // ROS_ERROR("No transform between base_link and camera_rgb_optical_frame found");
+      // ros::shutdown();
     }
-
 
     if (cloud_msg_received)
     {
       cloud_pub.publish(cloud_msg);
       cloud_msg_received = false;
     }
+
+    if (grasp_selected)
+    {
+      visualization_msgs::MarkerArray marker_array;
+      visualizeGraspCandidate(marker_array);
+      grasp_viz_pub.publish(marker_array);
+      grasp_selected = false;
+      break;
+    }
   }
 
+  spinner.start();
+
+
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to plan to the pose goal");
+
+  // Set pose goal from GPD
+  move_group.setGoalPositionTolerance(0.05);
+  // move_group.setGoalOrientationTolerance(0.05);
+  move_group.setPlanningTime(60.0);
+  grasp_pose_robot.pose.position.z += 0.2;
+  // move_group.setPoseTarget(grasp_pose_robot.pose);
+
+  move_group.setPositionTarget(grasp_pose_robot.pose.position.x,
+                               grasp_pose_robot.pose.position.y,
+                               grasp_pose_robot.pose.position.z);
+
+  // Compose motion plan
+  moveit::planning_interface::MoveGroupInterface::Plan plan;
+  bool success = (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  ROS_INFO_NAMED("GPD", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+  // Visualizing plans
+  ROS_INFO_NAMED("GPD", "Visualizing plan 1 as trajectory line");
+  visual_tools.publishAxisLabeled(grasp_pose_robot.pose, "graps pose");
+  visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.publishTrajectoryLine(plan.trajectory_, joint_model_group);
+  visual_tools.trigger();
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to move to the pose goal");
+
+  // Move to the goal
+  // move_group.move();
+  move_group.execute(plan);
+
+
+  ros::shutdown();
   return 0;
 }
 
