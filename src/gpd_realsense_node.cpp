c3e43d4a2f01aa0d67d00537421e9021f33c988d
bostoncleek <bocl8874@colorado.edu>
2020-07-01 10:24:38 -0600

    adding utilities lib for support

--- src/gpd_realsense_node.cpp
+++ src/gpd_realsense_node.cpp
@@ -16,22 +16,8 @@
 #include <std_srvs/Empty.h>
 #include <pcl_conversions/pcl_conversions.h>
 
-// C++
-#include <vector>
-#include <iostream>
-#include <string>
-
-// PCL
-#include <pcl/common/common.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/visualization/cloud_viewer.h>
+// Utils
+// #include "gpd_simulation/utils.hpp"
 
 
 static bool cloud_srv_active;                                   // request grasps status
