784ac5ce6f1e0437bb8c43f36ae5dfb5ac230cb4
bostoncleek <bocl8874@colorado.edu>
2020-07-01 10:23:49 -0600

    change name of executable

--- src/gpd_ur5_node.cpp
+++ src/gpd_ur5_node.cpp
@@ -36,30 +36,8 @@
 #include <moveit_msgs/CollisionObject.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
-// C++
-#include <vector>
-#include <iostream>
-#include <string>
-
-// PCL
-#include <pcl/common/common.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/visualization/cloud_viewer.h>
-
-// Eigen
-#include <Eigen/Dense>
-#include <Eigen/Geometry>
 
 // GPD
-// #include <gpd_ros/CloudSources.h>
-// #include <gpd_ros/CloudSamples.h>
 #include <gpd_ros/GraspConfig.h>
 #include <gpd_ros/GraspConfigList.h>
 
@@ -539,7 +517,7 @@ geometry_msgs::Pose postGrasp(const geometry_msgs::Pose &grasp, const geometry_m
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "gpd_sim");
+  ros::init(argc, argv, "gpd_ur5");
   ros::NodeHandle nh("~");
   ros::NodeHandle node_handle;
 
